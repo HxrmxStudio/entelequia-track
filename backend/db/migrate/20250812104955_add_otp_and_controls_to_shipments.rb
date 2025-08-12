0b57590c33e04fca30e02b1d451c7a525dbef78d
Emiliano Rozas <Emilianorozas@gmail.com>
2025-08-12 14:08:00 +0200

    feat (DB/Shipments): add OTP & geofence controls columns; TTL and token support; concurrent indexes

---
+++ backend/db/migrate/20250812104955_add_otp_and_controls_to_shipments.rb
@@ -0,0 +1,33 @@
+class AddOtpAndControlsToShipments < ActiveRecord::Migration[7.1]
+  disable_ddl_transaction!
+
+  def up
+    change_table :shipments do |t|
+      t.integer  :otp_attempts, default: 0, null: false
+      t.datetime :otp_locked_until
+      t.integer  :geofence_radius_m, default: 100, null: false
+      t.string   :otp_code_hash, limit: 128
+      t.datetime :otp_generated_at
+      t.datetime :otp_expires_at
+      t.string   :qr_token, limit: 128
+    end
+
+    add_index :shipments, :qr_token, unique: true, algorithm: :concurrently
+    add_index :shipments, :otp_expires_at, algorithm: :concurrently
+  end
+
+  def down
+    remove_index :shipments, :otp_expires_at
+    remove_index :shipments, :qr_token
+
+    change_table :shipments do |t|
+      t.remove :qr_token
+      t.remove :otp_expires_at
+      t.remove :otp_generated_at
+      t.remove :otp_code_hash
+      t.remove :geofence_radius_m
+      t.remove :otp_locked_until
+      t.remove :otp_attempts
+    end
+  end
+end
